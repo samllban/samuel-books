5cdc309df99ea72f22b58bebc94c6ebdd4f448a4
samllban <137834390+samllban@users.noreply.github.com>
2024-03-17 15:39:46 -0300

    conclusão do cap 02

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import Logo from './components/Logo';
-import perfil from './img/perfil.svg';
-import sacola from './img/sacola.svg';
-
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icones = [perfil, sacola];
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <ul className='opcoes'>
-          {textoOpcoes.map((texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-          ))}
-        </ul>
-        <ul className='icones'>
-            {icones.map((icone)=>(
-              <li className='icone'><img src={icone}></img></li>
-            ))}
-        </ul>
-      </header>
+      <Header />
     </div>
   );
 }
