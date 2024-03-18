2957b5f5dc268cea8816359e4988d1516049a589
samllban <137834390+samllban@users.noreply.github.com>
2024-03-18 16:26:32 -0300

    globla styles, fim do cap 03

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ const AppConteiner = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589);
-
-.li {
-  list-style: none;
-  
-}
 ` 
 function App() {
   return (
