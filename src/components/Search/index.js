7fd29fc0216d4014f439180f88a435eae541787d
samllban <137834390+samllban@users.noreply.github.com>
2024-03-19 15:26:55 -0300

    add os useStatus

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Input from '../Input';
 import { useState } from "react";
-
+import { livros } from './searchData';
 
 const SearchContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -24,9 +24,31 @@ const SubTitulo = styled.h3`
         font-weight: 500;
         margin-bottom: 40px; 
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    p {
+        width: 200px;
+
+    }
+
+    img {
+        width: 100px;
+    }
+
+    &:hover{
+        border: 1px solid white;
+    }
+
+`
+
 
 function Search() {
-    const [textoDigitado, setTextoDigitado] = useState('');
+    const [livrosPesquisados, setLivrosPesquisado] = useState([]);
 
     return (
         <SearchContainer>
@@ -34,9 +56,18 @@ function Search() {
             <SubTitulo>Encontre seu livro em nossa estante.</SubTitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
-                onBlur={evento => setTextoDigitado(evento.target.value)}
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value;
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisado(resultadoPesquisa);
+                }}
             />
-            <p>{textoDigitado}</p>
+            {livrosPesquisados.map(livro => (
+                <Resultado>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src} />
+                </Resultado>
+            ))}
         </SearchContainer>
     )
 }
